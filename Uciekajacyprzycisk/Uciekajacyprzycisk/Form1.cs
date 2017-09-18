18b758f29ad7b371ffd2083dab2c8d467a469a3f
walenty500 <32064444+walenty500@users.noreply.github.com>
2017-09-18 14:34:06 +0200

    Add files via upload

---
+++ Uciekajacyprzycisk/Uciekajacyprzycisk/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Uciekajacyprzycisk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Random los = new Random();
+            int x = los.Next(this.Width - button1.Width);
+            int y = los.Next(this.Height - button1.Height);
+
+            Point p = new Point(x, y);
+            button1.Location = p;
+
+        }
+    }
+}
